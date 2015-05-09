00ce8e3b8caf703783db10d11681afc3dc6a20ec
Yusuke Shimizu <a.ryuklnm@gmail.com>
2015-05-09 23:58:24 +0900

    colorize output with mgutz/ansi

--- emph.go
+++ emph.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/codegangsta/cli"
+	"github.com/mgutz/ansi"
+	"bufio"
+	"fmt"
 	"os"
 )
 
@@ -11,7 +14,15 @@ func main() {
 	app.Version = "0.1"
 	app.Usage = "The kind and sincere usage"
 	app.Action = func(c *cli.Context) {
-		println("he, he, hello?...")
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := scanner.Text()
+			colored_line := ansi.Color(line, "cyan+u")
+			fmt.Println(colored_line)
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
 	}
 
 	app.Run(os.Args)
